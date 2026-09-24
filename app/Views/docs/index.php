d444bb02c87335ffaa0eaa36411287523cc794d0
Abou Yaziyd <gakibrush@gmail.com>
2026-09-24 16:27:41 +0300

    docs

--- app/Views/docs/index.php
+++ app/Views/docs/index.php
@@ -11,6 +11,7 @@
                 <p><b>Jumla Jamala</b> <span class="badge badge-danger">3</span></p>
                 <a href="https://youtu.be/dP-PPdkRoJ4" class="btn btn-primary mb-1" target="_blank">001. Kuingia, Kutoka na Kubadili Lugha</a>
                 <a href="https://youtu.be/vyjjoOqIwvY" class="btn btn-primary mb-1" target="_blank">002. Ukurasa wa mwanzo</a>
+                <a href="https://youtube.com/shorts/mxd_pl9V__w" class="btn btn-primary mb-1" target="_blank">003. Kama umesahau Password</a>
                 <span class="btn btn-outline-primary mb-1">...</span>
             </div>
         </div>
